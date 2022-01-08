1a55b02e5fc4140ba1d6812941c7fa9a4c9c6231
W1nterFr3ak <byronchris25@gmail.com>
2022-01-08 10:07:52 +0300

    cases added

---
+++ cases.py
@@ -0,0 +1,15 @@
+class nMiss:
+	def __init__(self, a,b,c,miss):
+		self.a = a
+		self.b = b 
+		self.c = c
+		self.miss = miss 
+
+
+
+class Rieman:
+	def __init__(self, n, n_zeros, firsthalf, secondhalf):
+		self.n = n 
+		self.number_of_zeros = n_zeros 
+		self.first_half = firsthalf
+		self.second_half = secondhalf
